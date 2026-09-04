0ed632d98f1fe9bd462e436d0bb62556b03bb4d7
Amanjaved <amanjaved.aj@gmail.com>
2026-09-04 22:08:57 +0530

    Update Inventory Management System

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
